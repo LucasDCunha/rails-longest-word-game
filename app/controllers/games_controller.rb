770792c4590ff8c3b9f88164da3bb1567b6a5200
Lucas Cunha <lucasdinizcunha@uol.com.br>
2020-02-17 14:55:57 -0300

    Finish exercise two

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,37 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @start_time = Time.now.to_i
+    generate_letters
+    @letters
+  end
+
+  def score
+    get_score(params[:answer], params[:start_time], params[:letters])
+    @score
+  end
+
+  def generate_letters
+    @letters = []
+    10.times { @letters << ('a'..'z').to_a.sample }
+  end
+
+  def validate_answer(answer, letters)
+    validator = true
+    answer.chars.each do |char|
+      validator = false if answer.count(char) != letters.count(char)
+    end
+    validator
+  end
+
+  def get_score(answer, start_time, letters)
+    @score = 0
+    url = "https://wagon-dictionary.herokuapp.com/#{answer}"
+    api = JSON.parse(open(url).read)
+    if api['found'] == true && validate_answer(answer, letters)
+      @score = (answer.size * 100) / (Time.now.to_i - start_time.to_i)
+    end
+  end
+end
